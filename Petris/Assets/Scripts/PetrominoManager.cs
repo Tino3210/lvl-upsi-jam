4a4581114aee0339472d226dfb6ae982beaa85bf
piskel <10447278+piskel@users.noreply.github.com>
2022-05-28 10:27:52 +0200

    Avancement par pas de 1 et déplacement latéral des pièces

--- Petris/Assets/Scripts/PetrominoManager.cs
+++ Petris/Assets/Scripts/PetrominoManager.cs
@@ -12,9 +12,11 @@ public class PetrominoManager : MonoBehaviour
     }
 
     public Direction direction;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 1f;
     public bool isMoving = true;
     public bool isTrigger = false;
+
+    private float distanceTraveled = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,19 +27,44 @@ public class PetrominoManager : MonoBehaviour
     void Update()
     {
         if(isMoving){
+            float displacement = moveSpeed * Time.deltaTime;
+            float walkPiece = 0f;
+
+            distanceTraveled += displacement;
+
+            walkPiece = 0;
+
+            if (distanceTraveled > 1)
+            {
+                distanceTraveled = 0f;
+                walkPiece = 1;
+            }
+
+
+            float dashH = Input.GetKeyDown(KeyCode.D) ? 1 : 0;
+            dashH += Input.GetKeyDown(KeyCode.A) ? -1 : 0;
+
+
+            float dashV = Input.GetKeyDown(KeyCode.W) ? 1 : 0;
+            dashV += Input.GetKeyDown(KeyCode.S) ? -1 : 0;
+
+
+
             switch (direction)
             {
                 case Direction.North:
-                    transform.position = new Vector2(transform.position.x, transform.position.y + (moveSpeed  * Time.deltaTime));
+
+
+                    transform.position = new Vector2(transform.position.x + dashH, transform.position.y + walkPiece);
                 break;
                 case Direction.East:
-                    transform.position = new Vector2(transform.position.x + (moveSpeed  * Time.deltaTime), transform.position.y);
+                    transform.position = new Vector2(transform.position.x + walkPiece, transform.position.y + dashV);
                 break;
                 case Direction.South:
-                    transform.position = new Vector2(transform.position.x, transform.position.y - (moveSpeed  * Time.deltaTime));
+                    transform.position = new Vector2(transform.position.x + dashH, transform.position.y - walkPiece);
                 break;
                 case Direction.West:
-                    transform.position = new Vector2(transform.position.x - (moveSpeed  * Time.deltaTime), transform.position.y);
+                    transform.position = new Vector2(transform.position.x - walkPiece, transform.position.y + dashV);
                 break;            
             }
         }

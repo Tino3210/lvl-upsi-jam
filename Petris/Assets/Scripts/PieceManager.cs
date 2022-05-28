0ae57b97af215bf589023a26b33ae408438cebc6
Tino3210 <izzovalentino0@gmail.com>
2022-05-28 11:39:53 +0200

    petrominoooooooooooooooooooooooooooo

--- Petris/Assets/Scripts/PieceManager.cs
+++ Petris/Assets/Scripts/PieceManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PieceManager : MonoBehaviour
-{   
+{       
+
     void Start(){}
     void Update(){}
 
@@ -15,13 +16,12 @@ public class PieceManager : MonoBehaviour
         {            
             GameObject parent = gameObject.transform.parent.gameObject; 
             parent.GetComponent<PetrominoManager>().isTrigger = true;            
-            parent.GetComponent<PetrominoManager>().isMoving = false;
-            int nbChild = parent.transform.childCount;
-            for(int i = 0; i < nbChild; i++){
-                parent.transform.GetChild(0).tag = "plateform";
-                parent.transform.GetChild(0).SetParent(collision.transform);
-            }
-            Destroy(parent);
+            parent.GetComponent<PetrominoManager>().isMoving = false;            
+            
+            
+            GameObject.Find("Plateform").GetComponent<PlateformManager>().AddPetromino(parent);
+
+
             GameObject.Find("SpawnManager").GetComponent<SpawnManager>().SpawnPetronimo();
         }
     }
--- Petris/Assets/Scripts/PlateformManager.cs
+++ Petris/Assets/Scripts/PlateformManager.cs
@@ -4,17 +4,90 @@ using UnityEngine;
 
 public class PlateformManager : MonoBehaviour
 {
-    private GameObject[][] pieces;
+    [SerializeField]
+    private int sizeArray = 20;
+    private Dictionary<Vector2, GameObject> pieces;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pieces = new Dictionary<Vector2, GameObject>();        
+        /*pieces[(sizeArray/2)-1,(sizeArray/2)-1] = gameObject.transform.GetChild(0).gameObject;
+        pieces[(sizeArray/2)+1,(sizeArray/2)+1] = gameObject.transform.GetChild(1).gameObject;
+        pieces[(sizeArray/2)+1,(sizeArray/2)-1] = gameObject.transform.GetChild(2).gameObject;
+        pieces[(sizeArray/2),(sizeArray/2)] = gameObject.transform.GetChild(3).gameObject;*/
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AddPetromino(GameObject petromino){
+        int nbChild = petromino.transform.childCount;
+        for(int i = 0; i < nbChild; i++){
+            Transform piece = petromino.transform.GetChild(0);
+            piece.tag = "plateform";            
+            piece.SetParent(gameObject.transform);
+            AddPiece(piece.gameObject);      
+        }                        
+        Destroy(petromino);
+        CheckPerfectSquare();
+    }
+
+    private void AddPiece(GameObject piece){
+
+        // FIXME : Marche probablement pas
+        Vector2 roundedPos = new Vector2(Mathf.Round(piece.transform.position.x*2)/2, Mathf.Round(piece.transform.position.y*2)/2);
+
+        pieces.Add(roundedPos, piece);
+    }
+
+    private void CheckPerfectSquare(){
+        int maxLayer = sizeArray/2;
+        float halfPiece = 0.5f;
+        for(int layer = maxLayer; layer > 1; layer--){
+            bool isPetris = true;
+
+            for(int j = 0; j < 2*layer-1; j++){                
+                float layerIndex = layer-halfPiece;
+                if(!pieces.ContainsKey(new Vector2(layerIndex, layerIndex-j))){
+                    isPetris = false;
+                    break;
+                    }
+                if(!pieces.ContainsKey(new Vector2(layerIndex-j, -layerIndex))){
+                    isPetris = false;
+                    break;
+                }
+                if(!pieces.ContainsKey(new Vector2(-layerIndex+j, layerIndex))){
+                    isPetris = false;
+                    break;
+                }
+                if(!pieces.ContainsKey(new Vector2(-layerIndex, -layerIndex+j))){
+                    isPetris = false;
+                    break;
+                }                    
+            }
+
+            if(isPetris){
+                for(int j = 0; j < 2*layer-1; j++){                
+                    float layerIndex = layer-halfPiece;
+
+                    Destroy(pieces[new Vector2(layerIndex, layerIndex-j)]);
+                    Destroy(pieces[new Vector2(layerIndex-j, -layerIndex)]);
+                    Destroy(pieces[new Vector2(-layerIndex+j, layerIndex)]);
+                    Destroy(pieces[new Vector2(-layerIndex, -layerIndex+j)]);
+
+                    pieces.Remove(new Vector2(layerIndex, layerIndex-j));
+                    pieces.Remove(new Vector2(layerIndex-j, -layerIndex));
+                    pieces.Remove(new Vector2(-layerIndex+j, layerIndex));
+                    pieces.Remove(new Vector2(-layerIndex, -layerIndex+j));
+
+
+                  
+                }
+            }         
+        }
     }
 }
